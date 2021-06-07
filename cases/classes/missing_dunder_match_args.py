d479c536f38d7ef9826b5033cafb383b666809bb
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-06-07 18:27:54 +0300

    Missing __match_args__

---
+++ cases/classes/missing_dunder_match_args.py
@@ -0,0 +1,16 @@
+class Click:
+    # __match_args__ = ("position", "button")
+
+    def __init__(self, position, button):
+        self.position = position
+        self.button = button
+
+
+def main():
+    match Click(1, 2):
+        case Click(a, b):  # <- warning here
+            print(a, b)
+
+
+if __name__ == '__main__':
+    main()
